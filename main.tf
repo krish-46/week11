7067a29936d73a7e19b80334ce5f83d2eb96fee7
CHANDAN MAHATO <85212943+krish-46@users.noreply.github.com>
2025-07-13 00:33:14 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_security_group" "insecure_web_server" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.1/32"]
   }
 
   egress {
